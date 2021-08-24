0e1a89e9a225711c5258770bdc16f3e111b7ac26
Ben <dreamvb@outlook.com>
2021-08-24 23:23:26 +0100

    Version 1

---
+++ Shortcut Master/frmmain.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace Shortcut_Master
+{
+    partial class frmmain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstResults = new System.Windows.Forms.ListBox();
+            this.txtFind = new System.Windows.Forms.TextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.sTip1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblResults = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.statusStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lstResults
+            // 
+            this.lstResults.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstResults.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lstResults.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.lstResults.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstResults.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.lstResults.FormattingEnabled = true;
+            this.lstResults.IntegralHeight = false;
+            this.lstResults.ItemHeight = 16;
+            this.lstResults.Location = new System.Drawing.Point(8, 113);
+            this.lstResults.Name = "lstResults";
+            this.lstResults.Size = new System.Drawing.Size(286, 186);
+            this.lstResults.TabIndex = 1;
+            this.lstResults.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.lstResults_DrawItem);
+            this.lstResults.SelectedIndexChanged += new System.EventHandler(this.lstResults_SelectedIndexChanged);
+            this.lstResults.DoubleClick += new System.EventHandler(this.lstResults_DoubleClick);
+            this.lstResults.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.lstResults_KeyPress);
+            // 
+            // txtFind
+            // 
+            this.txtFind.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtFind.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txtFind.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtFind.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFind.Location = new System.Drawing.Point(4, 6);
+            this.txtFind.Multiline = true;
+            this.txtFind.Name = "txtFind";
+            this.txtFind.Size = new System.Drawing.Size(278, 20);
+            this.txtFind.TabIndex = 3;
+            this.txtFind.TextChanged += new System.EventHandler(this.txtFind_TextChanged);
+            this.txtFind.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtFind_KeyPress);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sTip1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 302);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(302, 22);
+            this.statusStrip1.TabIndex = 4;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // sTip1
+            // 
+            this.sTip1.Name = "sTip1";
+            this.sTip1.Size = new System.Drawing.Size(0, 17);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.SteelBlue;
+            this.panel1.Controls.Add(this.lblResults);
+            this.panel1.Location = new System.Drawing.Point(8, 79);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(157, 30);
+            this.panel1.TabIndex = 5;
+            // 
+            // lblResults
+            // 
+            this.lblResults.AutoSize = true;
+            this.lblResults.Font = new System.Drawing.Font("Lucida Console", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResults.ForeColor = System.Drawing.Color.White;
+            this.lblResults.Location = new System.Drawing.Point(6, 8);
+            this.lblResults.Name = "lblResults";
+            this.lblResults.Size = new System.Drawing.Size(77, 15);
+            this.lblResults.TabIndex = 0;
+            this.lblResults.Text = "Results";
+            // 
+            // panel3
+            // 
+            this.panel3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel3.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.panel3.Controls.Add(this.txtFind);
+            this.panel3.Location = new System.Drawing.Point(8, 38);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(286, 34);
+            this.panel3.TabIndex = 7;
+            this.panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.SteelBlue;
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(8, 6);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(157, 30);
+            this.panel2.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Console", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(6, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Search Query";
+            // 
+            // frmmain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(302, 324);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.lstResults);
+            this.Name = "frmmain";
+            this.ShowIcon = false;
+            this.Text = "Shortcut Master";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.frmmain_Shown);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstResults;
+        private System.Windows.Forms.TextBox txtFind;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblResults;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStripStatusLabel sTip1;
+    }
+}
+
---
+++ Shortcut Master/frmmain.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace Shortcut_Master
+{
+    public partial class frmmain : Form
+    {
+
+        private struct TFileDesc
+        {
+            public string FileTile;
+            public string FilePath;
+        }
+
+        private List<TFileDesc> _Files = null;
+        private List<TFileDesc> QueryResult = null;
+        private string RootPath = string.Empty;
+        private int lb_index = (-1);
+
+        public frmmain()
+        {
+            InitializeComponent();
+        }
+
+        private void UpdateStatus(List<TFileDesc> l)
+        {
+            if (l.Count == 0)
+            {
+                sTip1.Text = "Nothing found";
+            }
+            else
+            {
+                sTip1.Text = "Found " + l.Count().ToString() + " items";
+            }
+        }
+
+        private void OpenApp()
+        {
+            if (lb_index.Equals(-1)) { return; }
+
+            try
+            {
+                //Execute the application
+                ExecApp(QueryResult[lb_index].FilePath);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message,
+                    Text, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void ExecApp(string Filename)
+        {
+            Process p = new Process();
+            try
+            {
+                p.StartInfo.FileName = Filename;
+                p.Start();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, Text,
+                    MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void FillAppsList(string sFind)
+        {
+            //Clear list box
+            lstResults.Items.Clear();
+            QueryResult = new List<TFileDesc>();
+
+            if (sFind == string.Empty)
+            {
+                //Clear results
+                QueryResult.Clear();
+                //Clear list box items
+                lstResults.Items.Clear();
+            }
+            else
+            {
+                foreach (TFileDesc fd in _Files)
+                {
+                    //Check if a item in the fd.FileTile matches sFind
+                    if (fd.FileTile.IndexOf(sFind, StringComparison.OrdinalIgnoreCase) >= 0)
+                    {
+                        //Add item to results list
+                        QueryResult.Add(fd);
+                    }
+                }
+            }
+            //Clear list box
+            lstResults.Items.Clear();
+            //Fill list box with results
+            foreach (TFileDesc fd in QueryResult)
+            {
+                //Add item to list box.
+                lstResults.Items.Add(fd.FileTile);
+            }
+            UpdateStatus(QueryResult);
+        }
+
+        private void RefreshApps()
+        {
+            string[] Items = Directory.GetFiles(RootPath, "*.lnk", SearchOption.AllDirectories);
+
+            TFileDesc f_info;
+            int s_pos;
+
+            _Files = new List<TFileDesc>();
+            
+            foreach (string f in Items)
+            {
+                //Get file info
+                FileInfo fi = new FileInfo(f);
+                //Remove link from name
+                //Get position of the dot in the filename
+                s_pos = fi.Name.IndexOf('.');
+
+                if (s_pos != -1)
+                {
+                    //Extract up to the files .ext
+                    f_info.FileTile = fi.Name.Substring(0, s_pos);
+                    //Set filename
+                    f_info.FilePath = fi.FullName;
+                    //Add to list object
+                    _Files.Add(f_info);
+                }
+            }
+            Array.Clear(Items, 0, Items.Length);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            RootPath = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData) +
+                "\\Microsoft\\Windows\\Start Menu\\Programs\\";
+            //Get a list of all short cuts
+            RefreshApps();
+            UpdateStatus(_Files);
+        }
+
+        private void txtFind_TextChanged(object sender, EventArgs e)
+        {
+            FillAppsList(txtFind.Text.Trim());
+        }
+
+        private void lstResults_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lb_index = lstResults.SelectedIndex;
+        }
+
+        private void lstResults_DoubleClick(object sender, EventArgs e)
+        {
+            OpenApp();
+        }
+
+        private void lstResults_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Return)
+            {
+                OpenApp();
+            }
+        }
+
+        private void txtFind_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Return)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void frmmain_Shown(object sender, EventArgs e)
+        {
+            txtFind.Focus();
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void lstResults_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            bool isSelected = ((e.State & DrawItemState.Selected) == DrawItemState.Selected);
+
+            if (e.Index > -1)
+            {
+                /* If the item is selected set the background color to SystemColors.Highlight 
+                 or else set the color to either WhiteSmoke or White depending if the item index is even or odd */
+                Color color = isSelected ? Color.FromArgb(0,91,166) :
+                    e.Index % 2 == 0 ? Color.WhiteSmoke : Color.White;
+
+                // Background item brush
+                SolidBrush backgroundBrush = new SolidBrush(color);
+                // Text color brush
+                SolidBrush textBrush = new SolidBrush(e.ForeColor);
+
+                // Draw the background
+                e.Graphics.FillRectangle(backgroundBrush, e.Bounds);
+                // Draw the text
+                e.Graphics.DrawString(lstResults.Items[e.Index].ToString(), e.Font, textBrush, 12, e.Bounds.Y, 
+                    StringFormat.GenericTypographic);
+
+                // Clean up
+                backgroundBrush.Dispose();
+                textBrush.Dispose();
+            }
+            e.DrawFocusRectangle();
+        }
+    }
+}
